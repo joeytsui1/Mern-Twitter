b93a3b70a2f8648dcdd0770d31463aa77639ff68
Joey Tsui <joeytsui131@gmail.com>
2023-02-19 00:33:43 -0500

    created demo user

---
+++ backend/app.js
@@ -0,0 +1,62 @@
+const express = require('express');
+const cookieParser = require('cookie-parser');
+const logger = require('morgan');
+const csurf = require('csurf');
+const debug = require('debug');
+require('./models/User');
+const cors = require('cors');
+const { isProduction } = require('./config/keys');
+
+const usersRouter = require('./routes/api/users');
+const tweetsRouter = require('./routes/api/tweets')
+const csrfRouter = require('./routes/api/csrf');
+
+const app = express();
+
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+
+if (!isProduction) {
+  app.use(cors());
+}
+
+app.use(
+  csurf({
+    cookie: {
+      secure: isProduction,
+      sameSite: isProduction && "Lax",
+      httpOnly: true
+    }
+  })
+);
+
+app.use('/api/users', usersRouter);
+app.use('/api/tweets', tweetsRouter);
+app.use('/api/csrf', csrfRouter);
+
+
+app.use((req, res, next) => {
+  const err = new Error('Not Found');
+  err.statusCode = 404;
+  next(err);
+});
+
+const serverErrorLogger = debug('backend:error');
+
+// Express custom error handler that will be called whenever a route handler or
+// middleware throws an error or invokes the `next` function with a truthy value
+app.use((err, req, res, next) => {
+  serverErrorLogger(err);
+  const statusCode = err.statusCode || 500;
+  res.status(statusCode);
+  res.json({
+    message: err.message,
+    statusCode,
+    errors: err.errors
+  })
+});
+
+
+module.exports = app;
